31caf699243ffd0d9735e9722b3ee7cbc5463cf3
AlexII000 <alex_chon1@baylor.edu>
2023-03-15 14:34:22 -0500

    s

--- list_comprehension_start_file.py
+++ list_comprehension_start_file.py
@@ -16,65 +16,116 @@ for i in original_list:
     if filter(i):
         new_list.append(expressions(i))  '''
 
-#You can obtain the same thing using list comprehension:
+# You can obtain the same thing using list comprehension:
 
 # new_list = [expression(i) for i in original_list if filter(i)]
 
 
-#The list comprehension starts with a '[' and ']', to help you remember that the
-#result is going to be a list.
+# The list comprehension starts with a '[' and ']', to help you remember that the
+# result is going to be a list.
 
-#There are 3 parts to list comprehension:
+# There are 3 parts to list comprehension:
 
-#*result*  = [*transform/expression*    *iteration*         *filter*     ]
+# *result*  = [*transform/expression*    *iteration*         *filter*     ]
 
-#The filter part answers the question if the item should be transformed.
+# The filter part answers the question if the item should be transformed.
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+list2 = []
+
+for i in list1:
+    if i % 2 == 0:
+        y = i * 100
+        list2.append(y)
+
+list2 = [i*100 for i in list1 if i % 2 == 0]
+
+print(list2)
+
+x = [i for i in range(10)]
+print(x)
+
+x = [i**2 for i in range(10)]
+print(x)
+
+list1 = [3, 4, 5]
+list2 = [item*3 for item in list1]
+print(list2)
+
+listOfWords = ['this', 'is', 'a', 'list', 'of', 'words']
+items = [word for word in listOfWords]
+items = [word[0].upper() for word in listOfWords]
+print(items)
+
+z = [i**2 for i in range(11) if i % 2 == 1]
+print(z)
+
+string = "Hello 12345 World"
+v = [int(i) for i in string if i.isdigit()]
+print(v)
+
+numbers = []
+for i in string:
+    if i.isdigit():
+        numbers.append(int(i))
+
+a = [i for i in string if i.isalpha()]
+print(a)
+
+infile = open('test.txt', 'r')
+result = [i.rstrip('\n') for i in infile if "line3" in i]
+print(result)
 
 
+def double(x):
+    return x*2
 
+
+double_result = [double(x) for x in range(10)]
+print(double_result)
+
+result = [x+y for x in [10, 20, 30] for y in [20, 40, 60]]
+print(result)
 ## Exercise ##
 
-# 1 Using a list comprehension, create a new list called "newlist" out of the list "numbers", 
+# 1 Using a list comprehension, create a new list called "newlist" out of the list "numbers",
 # which contains only the positive numbers from the list, as integers.
 
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 
 
-
-
-## 2 create a list of integers which specify the length of each word in
-## a sentence except for the word 'the'
+# 2 create a list of integers which specify the length of each word in
+# a sentence except for the word 'the'
 
 sentence = "the quick brown fox jumps over the lazy dog"
 words = sentence.split()
 
 
+# Given dictionary is consisted of vehicles and their weights in kilograms.
+# Contruct a list of the names of vehicles with weight below 5000 kilograms.
+# In the same list comprehension make the key names all upper case.
 
-## Given dictionary is consisted of vehicles and their weights in kilograms. 
-## Contruct a list of the names of vehicles with weight below 5000 kilograms. 
-## In the same list comprehension make the key names all upper case.
+dict = {"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400,
+        "Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
 
-dict={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400, 
-"Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
-
-
-
-
-## Find all the numbers from 1 to 1000 that have a 4 in them
+b = [key for key in dict if dict[key] < 5000]
+print(b)
 
 
+# Find all the numbers from 1 to 1000 that have a 4 in them
+n = [i for i in range(1001) if '4' in str(i)]
+print(n)
 
-## count how many times the word 'the' appears in the text file - 'sometext.txt'
-
+# count how many times the word 'the' appears in the text file - 'sometext.txt'
+count = sum(line.count('the') for line in open('sometext.txt', 'r'))
+count = 0
+infile2 = open('sometext.txt', 'r')
+q = sum([i.count('the' or 'The') for i in infile2])
+print(q)
 
 
 ## Extract the numbers from the following phrase ##
-
-phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each " +
-"event, with about 3 or 4 that were classifled as serious per event.'
-
-
-
-
-
-
+phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each event, with about 3 or 4 that were classifled as serious per event.'
+a = [i for i in phrase if i.isdigit()]
+print(a)
